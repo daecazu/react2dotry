d7af04728d2821cbe1557dc81b5bcec8fadaf84a
daecazu <daecazu@gmail.com>
2016-05-29 22:24:48 -0500

    segundo commit

--- App.jsx
+++ App.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDom from 'react-dom'; 
 
+Resolutions = new Mongo.Collection("resolutions");
+
+
+
 export default class App extends React.Component{
+	
+	addResolution(event){
+		event.preventDefault();
+		var text = this.refs.resolution.value.trim();
+		
+		Resolutions.insert({
+			text:text, 
+			complete: false,
+			createdAt: new Date()
+		});
+		this.refs.resolution.value ="";
+	}
+
+
 	render(){ 
 		return(
-			<p>Hello World</p> 
+			<div>
+				<p>My Resolutions</p>
+				<form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
+					<input 
+						type="text"
+						ref="resolution"
+						placeholder="Finish React Meteor Series"/>
+				</form> 
+			</div>
 			)
 	}
 }
